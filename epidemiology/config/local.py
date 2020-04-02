651d6f157d64714e371485656b5aad676013557b
Akshay Peshave <peshave1@umbc.edu>
2020-04-02 11:21:00 -0400

    timeseries folder path moved to config as timeseries_rootpath

--- epidemiology/config/local.py
+++ epidemiology/config/local.py
@@ -1,3 +1,7 @@
+import os
+
+
 class FILESYSTEM:
   SRC_ROOT = '/home/axxe/Documents/Git_Repositories/kq/SARS-CoV-2/epidemiology/epidemiology'
   DATA_ROOT = '/home/axxe/Documents/Git_Repositories/kq/SARS-CoV-2/epidemiology/data'
+  TIMESERIES_ROOTPATH = os.path.join(DATA_ROOT, 'COVID-19', 'csse_covid_19_data', 'csse_covid_19_time_series')
