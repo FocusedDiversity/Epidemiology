d2e8b1a92afebf980823386ebd7d833af18e6117
Akshay Peshave <peshave1@umbc.edu>
2020-04-12 13:34:49 -0400

    updated setup script with metadata for future pypi release

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='epidem',
+  version='0.0.dev1',
+  packages=find_packages(),
+  include_package_data=True,
+  url='https://github.com/FocusedDiversity/Epidemiology',
+  license='Free for non-commercial use',  # TODO: possibles - MIT, GPL-3.0-only, LGPL-3.0
+  author='Synaptiq, Karotene, UMBC',
+  author_email='peshave.ak@gmail.com',
+  description='Epidemiology toolkit under-development',
+  install_requires=[
+    'numpy>=1.18.0,<1.19.0',
+    'scipy>=1.4.0,<1.5.0',
+    'pandas>=1.0.0,<1.1.0'
+  ],
+  keywords=['Epidemiology', 'Disease Spread Models', 'Disease Dashboards'],
+  classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Healthcare Industry',
+    'License :: Free for non-commercial use',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+  ]
+)
