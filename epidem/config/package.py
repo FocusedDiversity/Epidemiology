1898ecd0eed28aa38cc98e822111ccdd53648a04
Akshay Peshave <peshave1@umbc.edu>
2020-04-12 13:36:16 -0400

    single package config
    doing away with deployment environment based configs to enable future package release on pypi

---
+++ epidem/config/package.py
@@ -0,0 +1,9 @@
+import os
+import pathlib
+
+
+class FILESYSTEM:
+  PACKAGE_ROOT = pathlib.Path(__file__).parent.parent.parent.absolute()
+  REPO_ROOT = str(pathlib.Path(__file__).parent.absolute()).split('.venv')[0]
+  SRC_ROOT = os.path.join(PACKAGE_ROOT, 'epidem')
+  DATA_ROOT = os.path.join(REPO_ROOT, 'data')
--- epidemiology/config/dev.py
+++
@@ -1,9 +0,0 @@
-import os
-import pathlib
-
-
-class FILESYSTEM:
-  REPO_ROOT = pathlib.Path(__file__).parent.parent.parent.absolute()
-  SRC_ROOT = os.path.join(REPO_ROOT, 'epidemiology')
-  DATA_ROOT = os.path.join(REPO_ROOT, 'data')
-  TIMESERIES_ROOTPATH = os.path.join(DATA_ROOT, 'COVID-19', 'csse_covid_19_data', 'csse_covid_19_time_series')
